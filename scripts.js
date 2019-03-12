c819b4eb34d76457e61c1e66f8a7e79c0ce79ee1
Aaron Kulkarni <akulkarni@ucvts.org>
2019-03-12 13:19:38 -0400

    trying first push

--- scripts.js
+++ scripts.js
@@ -1,19 +1,27 @@
-  const canvas = document.getElementById('canvas');
-  const context = canvas.getContext("2d");
-  context.clearRect(0, 0, canvas.width, canvas.height);
+var boxWidth = 400;
+var boxHeight = 400;
+var padding = 10;
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const beginButton = document.getElementById('beginningbutton');
+drawBoard();
+context.clearRect(0, 0, canvas.width, canvas.height);
+  function drawBoard(){
+    for (var x = 0; x <= boxWidth; x += 40) {
+        context.moveTo(0.5 + x + padding, padding);
+        context.lineTo(0.5 + x + padding, boxHeight + padding);
+    }
 
-  function startGame() {
-  document.getElementById("homescreen").style.backgroundColor = "blue";
-  var homescreen = document.getElementById("homescreen");
-  if (homescreen.style.display === "none") {
-    homescreen.style.display = "block";
-  } else {
-    homescreen.style.display = "none";
-  }
+    for (var x = 0; x <= boxHeight; x += 40) {
+        context.moveTo(padding, 0.5 + x + padding);
+        context.lineTo(boxWidth + padding, 0.5 + x + padding);
+    }
+    context.strokeStyle = "black";
+    context.stroke();
+}
 
-  var gamescreen = document.getElementById("gamescreen")
-  if (gamescreen.style.display === "none" && homescreen.style.display === "none") {
-    gameplay();
+  function buttonClicked() {
+    beginButton.style.display = "none";
   }
 
   function gamePlay() {
@@ -23,4 +31,3 @@
   function placeApple() {
 
   }
-}
